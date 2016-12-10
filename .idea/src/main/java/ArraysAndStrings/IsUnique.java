0b679fdc2c0e160ae24a9480f42ceace45a729d7
smartlin5228 <smartlin1@gmail.com>
2016-12-09 17:24:02 -0800

    IsUnique function, and bit manupulation IsUnique function

--- .idea/src/main/java/ArraysAndStrings/IsUnique.java
+++ .idea/src/main/java/ArraysAndStrings/IsUnique.java
@@ -5,5 +5,27 @@ package main.java.ArraysAndStrings;
  * Hint: Ask interviewer if the string is an ASCII string or a Unicode string, normally assume it is ASCII string
  */
 public class IsUnique {
-  
+    public class Solution {
+        public boolean isUniqueChars(String string) {
+            if (string.length() > 128) return false;
+            boolean[] charsets = new boolean[128];
+            for (int i = 0; i < string.length(); i++) {
+                int val = string.charAt(i);
+                if (charsets[val]) return false;
+                charsets[val] = true;
+            }
+            return true;
+        }
+//        TODO: Bit Manipulation << , & , |
+        public boolean isUniqueCharsBit(String string) {
+            int checker = 0;
+            for (int i = 0; i < string.length(); i++) {
+                int val = string.charAt(i) - 'a';
+                if ((checker & (1 << val)) > 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
